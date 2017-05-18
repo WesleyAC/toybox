7685ca4057e0fbb77df3162345ce334238710d4f
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-05-18 05:44:55 -0700

    Add visualization function to neuralnets/net.py

--- neuralnets/net.py
+++ neuralnets/net.py
@@ -7,7 +7,9 @@ def sigmoid_prime(x):
     return x*(1-x)
 
 class NeuralNet(object):
-    def __init__(self, num_input, num_hidden_layers, num_neurons_per_layer, num_output):
+    def __init__(self, num_input, num_hidden_layers, num_neurons_per_layer, num_output, seed=-1):
+        if seed != -1:
+            np.random.seed(seed)
         self.synapses = []
 
         self.synapses.append(2*np.random.random((num_input, num_neurons_per_layer)) - 1)
@@ -47,3 +49,31 @@ class NeuralNet(object):
         output_error = outputs - results[-1]
 
         return np.mean(np.abs(output_error))
+
+    def output_dot(self, error=None):
+        output = []
+        output.append("digraph {")
+        for index,synapse in enumerate(self.synapses):
+            if index == 0:
+                prefix = "I"
+                nextprefix = "H"
+            elif index == len(self.synapses)-1:
+                prefix = "H"
+                nextprefix = "O"
+            else:
+                prefix = "H"
+                nextprefix = "H"
+            for row in xrange(synapse.shape[0]):
+                for col in xrange(synapse.shape[1]):
+                    output.append("{}{} -> {}{} [color=\"{},{},0.6\"]".format(
+                        prefix, row,
+                        nextprefix, col,
+                        0.0 if np.sign(synapse[row][col]) == -1 else 0.7,
+                        round((sigmoid(np.abs(synapse[row][col]))-0.5)*2, 4),
+                    ))
+        if error is not None:
+            output.append("error [label=\"Error:\n{}%\", shape=plaintext]".format(round(self.error(error[0], error[1])*100, 2)))
+            output.append("O0 -> error [style=invis]")
+            output.append("O{} -> error [style=invis]".format(self.synapses[-1].shape[1]-1))
+        output.append("}")
+        return "\n".join(output)
