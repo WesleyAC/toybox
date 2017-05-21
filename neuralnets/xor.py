0c35e3ecd5e4c28a8a72d5300d80b8db50d17b5b
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-05-21 15:52:30 -0700

    Add visualization option to xor neuralnet

--- neuralnets/xor.py
+++ neuralnets/xor.py
@@ -1,7 +1,8 @@
 import numpy as np
 from net import NeuralNet
 
-net = NeuralNet(2, 1, 3, 1)
+net = NeuralNet(2, 1, 3, 1, 342047)
+output_dot = True
 
 inputs = np.array([[1,1],
                    [0,0],
@@ -12,13 +13,11 @@ outputs = np.array([[0],
                     [0],
                     [1],
                     [1]])
-print("untrained")
-print("error: {}".format(net.error(inputs, outputs)))
-for inpt in inputs:
-    print(net.forward(inpt))
 
-for i in xrange(100000):
-    net.learn(inputs, outputs, 1.0)
+for i in xrange(50000):
+    if i % 100 == 0 and output_dot:
+        open("/tmp/xor{:05d}graph".format(i), mode="w").write(net.output_dot((inputs,outputs)))
+    net.learn(inputs, outputs, 0.05)
 
 print("trained")
 print("error: {}".format(net.error(inputs, outputs)))
