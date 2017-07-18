b8c0cd2e85558241d9ce9b86b1407c650e4c4646
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-18 17:48:55 -0400

    Add encoder mask to pcad

--- pcad/gen.py
+++ pcad/gen.py
@@ -9,7 +9,8 @@ if len(sys.argv) < 2:
 loader = TemplateLoader(os.path.dirname(__file__))
 tmpl = loader.load(sys.argv[1])
 print(tmpl.generate(r_outer=100,
-                    r_inner=50,
+                    r_inner=25,
                     spacing=3,
                     num_clicks=16,
+                    flip=True,
                     debug=False))
