257c04000c85063136f57d069cc11008703e5a96
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-25 21:17:50 -0400

    Implement buffer read and write as circular buffers

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -12,25 +12,37 @@ struct buffer {
   int head;
 };
 
-int buffer_free_space(buffer buf) {
-  if (buf.head > buf.tail) {
-    return (buf.size-1) - (buf.head - buf.tail);
+int buffer_free_space(buffer *buf) {
+  if (buf->head > buf->tail) {
+    return (buf->size-1) - (buf->head - buf->tail);
+  } else if (buf->head < buf->tail) {
+    return (buf->size+1) - (buf->tail - buf->head);
   } else {
-    return (buf.size+1) - (buf.tail - buf.head);
+    return buf->size;
   }
 }
 
-int buffer_put(buffer buf, char *data, int size) {
+int buffer_used_space(buffer *buf) {
+  return buf->size - buffer_free_space(buf);
+}
+
+int buffer_put(buffer *buf, char *data, int size) {
   size = MIN(buffer_free_space(buf), size);
-  int end = (buf.head < buf.tail) ? buf.tail : buf.size;
-  int first_write_size = MIN(end - buf.head, size);
-  memcpy(buf.buf + buf.head, data, first_write_size);
-  memcpy(buf.buf, data + first_write_size, size - first_write_size);
+  int end = (buf->head < buf->tail) ? buf->tail : buf->size;
+  int first_write_size = MIN(end - buf->head, size);
+  memcpy(buf->buf + buf->head, data, first_write_size);
+  memcpy(buf->buf, data + first_write_size, size - first_write_size);
+  buf->head = (buf->head + size) % buf->size;
   return size;
 }
 
-int buffer_get(buffer buf, char *data, int size) {
-  memcpy(data, buf.buf, size);
+int buffer_get(buffer *buf, char *data, int size) {
+  size = MIN(buffer_used_space(buf), size);
+  int end = (buf->head < buf->tail) ? buf->size : buf->head;
+  int first_read_size = MIN(end - buf->tail, size);
+  memcpy(data, buf->buf + buf->tail, first_read_size);
+  memcpy(data, buf->buf + buf->tail + first_read_size, size - first_read_size);
+  buf->tail = (buf->tail + size) % buf->size;
   return size;
 }
 
@@ -42,8 +54,8 @@ int main() {
   char write_data[4] = {42, 13, 7, 21};
   char read_buffer[4];
 
-  buffer_put(buf, write_data, 4);
-  buffer_get(buf, read_buffer, 4);
+  buffer_put(&buf, write_data, 4);
+  buffer_get(&buf, read_buffer, 4);
 
   printf("%d %d %d %d", read_buffer[0], read_buffer[1], read_buffer[2], read_buffer[3]);
 }
