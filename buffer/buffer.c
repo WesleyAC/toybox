a2ff5e603efa1cfbe56ac07d60fac89f902b7cbb
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-26 11:54:39 -0400

    Fix buffer read/write when tail is ahead of head

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -29,7 +29,7 @@ int buffer_put(buffer *const buf, const char *const data, int size) {
   int end = (buf->head < buf->tail) ? buf->tail : BUFFER_SIZE;
   int first_write_size = MIN(end - buf->head, size);
   memcpy(buf->buf + buf->head, data, first_write_size);
-  memcpy(buf->buf, data + first_write_size, size - first_write_size);
+  memcpy(buf->buf + buf->head + first_write_size, data + first_write_size, size - first_write_size);
   buf->head = (buf->head + size) % BUFFER_SIZE;
   return size;
 }
@@ -39,7 +39,7 @@ int buffer_get(buffer *const buf, char *const data, int size) {
   int end = (buf->head < buf->tail) ? BUFFER_SIZE : buf->head;
   int first_read_size = MIN(end - buf->tail, size);
   memcpy(data, buf->buf + buf->tail, first_read_size);
-  memcpy(data, buf->buf + buf->tail + first_read_size, size - first_read_size);
+  memcpy(data + first_read_size, buf->buf + buf->tail + first_read_size, size - first_read_size);
   buf->tail = (buf->tail + size) % BUFFER_SIZE;
   return size;
 }
@@ -48,11 +48,11 @@ int buffer_get(buffer *const buf, char *const data, int size) {
 int main() {
   buffer buf = { 0, 0, 0 };
 
-  char write_data[4] = {42, 13, 7, 21};
-  char read_buffer[4];
+  char write_data[9] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
+  char read_buffer[20];
 
-  buffer_put(&buf, write_data, 4);
-  buffer_get(&buf, read_buffer, 4);
+  buffer_put(&buf, write_data, 9);
+  buffer_get(&buf, read_buffer, 20);
 
   printf("%d %d %d %d", read_buffer[0], read_buffer[1], read_buffer[2], read_buffer[3]);
 }
