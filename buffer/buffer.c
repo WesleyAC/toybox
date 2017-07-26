e36af96e9a543b9fed5aed621ffffb8bf555b28d
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-26 09:10:47 -0400

    Remove size option from buffer and change buffer to not be a pointer

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -6,50 +6,48 @@
 
 typedef struct buffer buffer;
 struct buffer {
-  char *buf;
-  int size;
+  char buf[BUFFER_SIZE];
   int tail;
   int head;
 };
 
 int buffer_free_space(buffer *buf) {
   if (buf->head > buf->tail) {
-    return (buf->size-1) - (buf->head - buf->tail);
+    return (BUFFER_SIZE-1) - (buf->head - buf->tail);
   } else if (buf->head < buf->tail) {
-    return (buf->size+1) - (buf->tail - buf->head);
+    return (BUFFER_SIZE+1) - (buf->tail - buf->head);
   } else {
-    return buf->size;
+    return BUFFER_SIZE;
   }
 }
 
 int buffer_used_space(buffer *buf) {
-  return buf->size - buffer_free_space(buf);
+  return BUFFER_SIZE - buffer_free_space(buf);
 }
 
 int buffer_put(buffer *buf, char *data, int size) {
   size = MIN(buffer_free_space(buf), size);
-  int end = (buf->head < buf->tail) ? buf->tail : buf->size;
+  int end = (buf->head < buf->tail) ? buf->tail : BUFFER_SIZE;
   int first_write_size = MIN(end - buf->head, size);
   memcpy(buf->buf + buf->head, data, first_write_size);
   memcpy(buf->buf, data + first_write_size, size - first_write_size);
-  buf->head = (buf->head + size) % buf->size;
+  buf->head = (buf->head + size) % BUFFER_SIZE;
   return size;
 }
 
 int buffer_get(buffer *buf, char *data, int size) {
   size = MIN(buffer_used_space(buf), size);
-  int end = (buf->head < buf->tail) ? buf->size : buf->head;
+  int end = (buf->head < buf->tail) ? BUFFER_SIZE : buf->head;
   int first_read_size = MIN(end - buf->tail, size);
   memcpy(data, buf->buf + buf->tail, first_read_size);
   memcpy(data, buf->buf + buf->tail + first_read_size, size - first_read_size);
-  buf->tail = (buf->tail + size) % buf->size;
+  buf->tail = (buf->tail + size) % BUFFER_SIZE;
   return size;
 }
 
 
 int main() {
-  char data[BUFFER_SIZE];
-  buffer buf = { data, BUFFER_SIZE, 0, 0 };
+  buffer buf = { 0, 0, 0 };
 
   char write_data[4] = {42, 13, 7, 21};
   char read_buffer[4];
