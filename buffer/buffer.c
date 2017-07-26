a0a17aa2eaaf6c88515a0b6cc867df48c14bce82
Wesley Aptekar-Cassels <W.Aptekar@gmail.com>
2017-07-25 20:40:57 -0400

    Implement buffer_put as a real circular buffer

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define BUFFER_SIZE 255
 
@@ -10,11 +11,31 @@ struct buffer {
   int head;
 };
 
+int buffer_free_space(buffer buf) {
+  if (buf.head > buf.tail) {
+    return (buf.size-1) - (buf.head - buf.tail);
+  } else {
+    return (buf.size+1) - (buf.tail - buf.head);
+  }
+}
+
 int buffer_put(buffer buf, char *data, int size) {
+  int free_space = buffer_free_space(buf);
+  if (size > free_space) {
+    size = free_space;
+  }
+  int end = (buf.head < buf.tail) ? buf.tail : buf.size;
+  int first_write_size = end - buf.head;
+  if (first_write_size > size) {
+    first_write_size = size;
+  }
+  memcpy(buf.buf + buf.head, data, first_write_size);
+  memcpy(buf.buf, data + first_write_size, size - first_write_size);
   return size;
 }
 
 int buffer_get(buffer buf, char *data, int size) {
+  memcpy(data, buf.buf, size);
   return size;
 }
 
